34b5aee858645290f953d45d58160cbc147fc7bd
Daniel <daniil.orlov.ua@gmail.com>
2025-03-07 08:54:47 -0500

    update system operations

--- week9/sys_operations.py
+++ week9/sys_operations.py
@@ -1,5 +1,23 @@
 import platform
+import socket
+import os
 
 print("Current Machine Type")
 print(platform.machine())
+print("==========================")
+
+print("Current Processor Type")
+print(platform.architecture)
+print("==========================")
+
+print("Set Socket Timeout to 50 Seconds")
+print(socket.setdefaulttimeout(50))
+print("Get the current Socket Timeout")
+print(socket.getdefaulttimeout())
+print("==========================")
+
+print("Get current Operating System Type")
+print(os.name)
+print("Get current Operating System Name")
+print(platform.name)
 print("==========================")
