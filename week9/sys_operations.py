6efa94b0eea39147265e2437d6685d6856f9f44f
Daniel <daniil.orlov.ua@gmail.com>
2025-03-07 09:02:18 -0500

    add file practice, file handle to repo

--- week9/sys_operations.py
+++ week9/sys_operations.py
@@ -21,3 +21,18 @@ print(os.name)
 print("Get current Operating System Name")
 print(platform.name)
 print("==========================")
+
+print("Current Process ID")
+print(os.getpid())
+print("==========================")
+
+file_name = "fdpractice.txt"
+print(f"\n[Before Fork] Process {os.getpid()}")
+
+file_handle = os.open(file_name, os.O_RDWR | os.O_CREAT)
+print(f"\n[Process {os.getpid()}] Opened file_handle: {file_handle}")
+
+file_object_TextIO = os.fdopen(file_handle, "w+")
+
+file_object_TextIO.write("Some string to write to the file")
+file_object_TextIO.flush()
