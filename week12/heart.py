c8fc11e6594d57d0898af805387234458b673761
Daniel <daniil.orlov.ua@gmail.com>
2025-03-28 08:52:29 -0400

    fix bugs

--- week12/heart.py
+++ week12/heart.py
@@ -1,21 +1,21 @@
 # Heart class for composition
 class Heart:
-    def __init__(self, bpm=72):
-        self.__bpm = bpm
+    def __init__(self, beats_per_minute=72):
+        self.__beats_per_minute = beats_per_minute
     
     @property
-    def bpm(self):
-        return self.__bpm
+    def beats_per_minute(self):
+        return self.__beats_per_minute
     
-    @bpm.setter
-    def bpm(self, bpm):
-        self.__bpm = bpm
+    @beats_per_minute.setter
+    def beats_per_minute(self, beats_per_minute):
+        self.__beats_per_minute = beats_per_minute
 
     def beat(self):
         print("Lub-dub")
 
     def __str__(self):
-        return f"Heart is beating at {self.bpm} bpm), "
+        return f"Heart is beating at {self.beats_per_minute} bpm), "
     
     def __del__(self):
         print("Heart is deleted by a destructor")
