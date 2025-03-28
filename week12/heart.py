411023f82543a07c280b9fbc4158d0f7da136a32
Daniel <daniil.orlov.ua@gmail.com>
2025-03-28 08:46:31 -0400

    update heart

--- week12/heart.py
+++ week12/heart.py
@@ -1,3 +1,18 @@
 # Heart class for composition
 class Heart:
-    pass
+    def __init__(self, bpm=72):
+        self.__bpm = bpm
+    
+    @property
+    def bpm(self):
+        return self.__bpm
+    
+    @bpm.setter
+    def bpm(self, bpm):
+        self.__bpm = bpm
+
+    def beat(self):
+        print("Lub-dub")
+
+    def __str__(self):
+        return f"Heart is beating at {self.bpm} bpm), "
